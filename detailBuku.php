ce4bb50c45c13f17de5c2b459be9ab8d8f8d962a
Valian <valvilvul@gmail.com>
2016-12-01 14:42:37 +0700

    mengedit halaman login

--- detailBuku.php
+++ detailBuku.php
@@ -44,6 +44,14 @@
 				</div>
 			</nav>
 		</div>
+		<div class="col-md-offset-5 col-md-4">
+			<h1>Bodo Amat Ah</h1>
+		</div>
+		<div>
+			<?php
+				
+			?>
+		</div>
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>	
--- login.php
+++ login.php
@@ -1,5 +1,47 @@
 <?php
-	
+	function connectDB() {
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "tugasAkhir";
+		
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+		
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " + mysqli_connect_error());
+		}
+		return $conn;
+	}
+
+	function login(){
+        $conn = connectDB();
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT id, username, password, fullname, email, role FROM user";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+
+        }
+        else {
+            die("Error: $sql");
+        }
+
+        foreach ($result as $value) {
+            if($value['username']==$username && $value['password']==$password) {
+                if($value['role'] == 'admin'){
+                    header("Location: admin.php");
+                }
+            }
+        }
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+       
+    }
 
 ?>
 <!DOCTYPE html>
@@ -18,12 +60,14 @@
 		    <br>
 		    <br>
 		    <br>
+		    <br>
+		    <br>
 		        <div class="col-md-offset-5 col-md-4">
 		            <div class="form-login">
-		            	<h4>Login</h4>
+		            	<h4>Login Perpustakaan</h4>
 		            	<input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username" />
 		            	</br>
-		            	<input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
+		            	<input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
 		            	</br>
 		            	<div class="wrapper">
 		            		<span class="group-btn">     
