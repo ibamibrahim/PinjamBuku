262bcb20ace2740b0e92bb223d40e27cb12761aa
Valian <valvilvul@gmail.com>
2016-12-03 21:41:41 +0700

    mengubah tampilan buku dan menambahkan halaman penambahan buku admin

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<head>
 		<title>PinjamBuku</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="src/css/bootstrap/dist/css/bootstrap.css">
         <style>
         body{
             background-color: #eeeeee;
@@ -50,8 +50,6 @@
 					<div class="navbar-header">
       					<a class="navbar-brand" href="#">PinjamBuku</a>
     				</div>
-    				
-    				
                     <?php 
     					if($loggedIn) {
     						echo '<div class="navbar-nav navbar-right">
